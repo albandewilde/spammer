c1ae63046b2eb76326bdd2bee671d6375f720574
albandewilde <dewilde@intechinfo.fr>
2020-01-09 01:26:12 +0100

    Improve the logger

--- spammer.rb
+++ spammer.rb
@@ -10,7 +10,7 @@ end
 
 def waiting_send(bot, channel, message, wait)
   # Wait the time before sending the message
-  puts "I'm now waiting #{wait} secondes."
+  puts "I'm now waiting \033[92m#{wait}\033[0m secondes."
   sleep(wait)
 
   # Browse the list of server we are
@@ -20,7 +20,7 @@ def waiting_send(bot, channel, message, wait)
       # If the channel have the good name, we send the message
       if channel == chan.name
         # Send message
-        puts "I'm sending the message `#{message}` the the channel #{chan.name} of the server #{serv.name}"
+        puts "I'm sending the message `\033[94m#{message}\033[0m` the the channel \033[35m#{chan.name}\033[0m of the server \033[35m#{serv.name}\033[0m"
         bot.send_message(chan, message)
 
         # Don't break, it may have multilple channels with the same name
@@ -36,7 +36,7 @@ token = JSON.parse(File.read("secrets.json"))["discord"]
 # Create the bot
 bot = Discordrb::Bot.new(token: token)
 bot.run(background: true)
-puts "I'm logged in, this is my invite link → #{bot.invite_url} ←"
+puts "I'm logged in, this is my invite link \033[5:31m→\033[0m \033[93m#{bot.invite_url}\033[0m \033[5:31m←\033[0m"
 
 # The infinite loop of the bot
 while true
